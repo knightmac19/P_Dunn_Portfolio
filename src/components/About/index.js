10f434012bb3f7c9a14829be59591c8ed9a6c62a
Patrick Dunn <pmdunn78@gmail.com>
2020-04-16 16:51:14 -0400

    add imgs, story, header

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,31 @@
 import React from "react";
 import "./style.css";
 import ipad from "../img/ipad.png";
+import chazy from "../img/chazy.JPG";
+import engelberg from "../img/engelberg.JPG";
+import adirondacks from "../img/adirondacks.JPG";
+import huasteca from "../img/huasteca.JPG";
+import teotihuacan from "../img/teotihuacan.JPG";
+import chazyGray from "../img/chazyGray.jpg";
+import engelbergGray from "../img/engelbergGray.jpg";
+import adirondacksGray from "../img/adirondacksGray.jpg";
+import huastecaGray from "../img/huastecaGray.jpg";
+import teotihuacanGray from "../img/teotihuacanGray.jpg";
 
+
+
+// url("../img/bg-masthead.jpg"); 
+   
+// url("../img/adirondacks.JPG");
+   
+// url("../img/chazy.JPG");
+  
+// url("../img/engelberg.JPG");
+ 
+// url("../img/huasteca.JPG");
+   
+// url("../img/teotihuacan.JPG");
+{/* <img src={teotihuacan} className="img-fluid" alt="" /> */}
 
 function About() {
   return <div>
@@ -9,12 +33,29 @@ function About() {
       <div className="container">
         <div className="row">
           <div className="col-lg-8 mx-auto">
-            <h2 className="text-white mb-4">Built with Bootstrap 4</h2>
-            <p className="text-white-50">Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
-              <a href="http://startbootstrap.com/template-overviews/grayscale/">the preview page</a>. The theme is open source, and you can use it for any purpose, personal or commercial.</p>
+            <h2 className="text-white mb-4">My Story</h2>
+            <p className="text-white-50">
+              I've worked as a carpenter, a translator, and an English teacher in Mexico. I enjoy building elegant systems, learning new things, and improving myself. In November 2019 I began studying to become a Web Developer.
+            </p>
           </div>
         </div>
-        <img src={ipad} className="img-fluid" alt="" />
+        <div className="row">
+          <div className="col-lg-8 mx-auto">
+            <h2 className="text-white mb-4">My Story</h2>
+            <p className="text-white-50">
+              Changing careers wasn't easy. The intensive bootcamp I took required me to be comfortable with ambiguity during the learning process. Some weeks I felt I was barely keeping my head above water. 
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-8 mx-auto">
+            <h2 className="text-white mb-4">My Story</h2>
+            <p className="text-white-50">
+              Over time the hours of studying paid off. From an HTML-novice I grew into a Full Stack Developer with a robust portfolio. What used to be unkown and overwhelming -- DOM manipulation, asynchronous functions, the MERN stack -- have become trusted tools. 
+            </p>
+          </div>
+        </div>
+        
       </div>
     </section>
   </div>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,9 +6,10 @@ function Header() {
     <header className="masthead">
       <div className="container d-flex h-100 align-items-center">
         <div className="mx-auto text-center">
-          <h1 className="mx-auto my-0 text-uppercase">Grayscale</h1>
-          <h2 className="text-white-50 mx-auto mt-2 mb-5">A free, responsive, one page Bootstrap theme created by Start Bootstrap.</h2>
-          <a href="#about" className="btn btn-primary js-scroll-trigger">Get Started</a>
+          <h1 className="mx-auto my-0 text-uppercase">Patrick Dunn</h1>
+          <h2 className="text-white-50 mx-auto mt-2 mb-2">Full Stack Web Developer</h2>
+          <h2 className="text-white-50 mx-auto mt-2 mb-5">HTML · CSS · jQuery · mySQL · MongoDB · Express · React · Node.js</h2>
+          <a href="#about" className="btn btn-primary js-scroll-trigger">My Story</a>
         </div>
       </div>
     </header>
