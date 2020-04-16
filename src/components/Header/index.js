7c8d601fb96d9f462ffaba40df50544d8d6da5f0
Patrick Dunn <pmdunn78@gmail.com>
2020-04-16 19:38:11 -0400

    playing with header text

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,9 +7,9 @@ function Header() {
       <div className="container d-flex h-100 align-items-center">
         <div className="mx-auto text-center">
           <h1 className="mx-auto my-0 text-uppercase">Patrick Dunn</h1>
-          <h2 className="text-white mx-auto mt-2 mb-3">FULL STACK WEB DEVELOPER</h2>
-          <h2 className="text-white mx-auto mt-2 mb-0">HTML · CSS · jQuery · mySQL · MongoDB</h2>
-          <h2 className="text-white mx-auto mt-2 mb-5">Express · React · Node.js</h2>
+          <h2 className="text-white mx-auto mt-2 mb-5">FULL STACK WEB DEVELOPER</h2>
+          <h2 className="text-white mx-auto mt-2 mb-0">HTML · CSS · Javascript · jQuery · mySQL</h2>
+          <h2 className="text-white mx-auto mt-2 mb-5">MongoDB · Express · React · Node.js</h2>
           <a href="#about" className="btn btn-primary js-scroll-trigger">My Story</a>
         </div>
       </div>
