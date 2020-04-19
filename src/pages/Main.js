7ba1bb10dba2f7df0048a59b8babb1b61fea01af
Patrick Dunn <pmdunn78@gmail.com>
2020-04-18 20:33:03 -0400

    removing scroll event class component

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -10,42 +10,6 @@ import Footer from "../components/Footer";
 
 
 class Main extends Component {
-
-  state = {
-    isTop: true,
-    
-  };
-  
-
-
-  navbarCollapse = function() {
-    console.log("navbarCollapse");
-    var mainNav = document.getElementById("mainNav");
-    if (window.scrollY < 100) {
-      mainNav.removeAttribute("class", "navbar-shrink");
-    } else {
-      mainNav.setAttribute("class", "navbar-shrink");
-    }
-    
-  }
-
-  navbarUncollapse = function() {
-    return this.state.mainNav.removeAttribute("class", "navbar-shrink");
-  }
-  
-  stackScroll = function() {
-    document.addEventListener("scroll", () => {
-      const isTop = window.scrollY < 100;
-      if (isTop !== this.state.isTop) {
-        this.setState({ isTop })
-      }    
-    });
-  }
-
-  componentDidMount() {
-    // this.navbarCollapse()
-    this.stackScroll()
-  }
   
   render() {
     return (
